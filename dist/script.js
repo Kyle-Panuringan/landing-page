b1491defcf2fb6866a5a9ac0018092e0002afde3
Kyle Panuringan <kylepanuringan@gmail.com>
2022-07-16 20:32:32 +0800

    Add enroll.html

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,15 @@
 const hamburger = document.querySelector("#hamburger");
 const nav = document.querySelector("nav");
-hamburger.addEventListener("click", () => {
+
+const showNav = () => {
 	nav.classList.toggle("show-links");
-});
+};
+
+const autoHideNav = () => {
+	if (window.scrollY >= 200) {
+		nav.classList.remove("show-links");
+	}
+};
+
+window.addEventListener("scroll", autoHideNav);
+hamburger.addEventListener("click", showNav);
